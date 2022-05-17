fdaae583f790b6c75899218a7a98b9e02491de12
MilesChou <jangconan@gmail.com>
2022-05-17 17:27:53 +0800

    Add Laravel Zero

---
+++ app/Commands/Suite.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Commands;
+
+use Illuminate\Console\Command;
+
+class Suite extends Command
+{
+    protected $signature = 'suite';
+
+    protected $description = '';
+
+    public function handle(): int
+    {
+
+        return 0;
+    }
+}
